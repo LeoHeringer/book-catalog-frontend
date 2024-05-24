cfcf784bf0bb4b535efdfcecab20a9c0a1ab954b
LeoHeringer <leonardoheringer2016@hotmail.com>
2024-05-24 03:27:19 -0300

    css

--- src/pages/home/index.js
+++ src/pages/home/index.js
@@ -1,6 +1,40 @@
+import { Container, BookList, Book } from "./styles.js";
+
 function Home () {
+    
+    const books = [
+        {
+            id: 1,
+            title: 'Dracula',
+            image_url: 'https://m.media-amazon.com/images/I/51Mg0vMecsL._AC_UF1000,1000_QL80_.jpg'
+        },
+        {
+            id: 2,
+            title: 'Le',
+            image_url: 'https://m.media-amazon.com/images/I/51Mg0vMecsL._AC_UF1000,1000_QL80_.jpg'
+        },
+        {
+            id: 3,
+            title: 'Lu',
+            image_url: 'https://m.media-amazon.com/images/I/51Mg0vMecsL._AC_UF1000,1000_QL80_.jpg'       
+        }
+
+    ]
     return (
-        <h1>Hello world</h1>
+        <Container>
+        <h1>Books</h1>
+        <BookList>
+            {books.map(books => {
+                return (
+                    <Book>
+                        <a href="https://google.com.br"><img src={books.image_url} all={books.title}/></a>
+                        <span>{books.title}</span>
+                    </Book>
+                )
+            })}
+        </BookList>
+        </Container>
+
     )
 }
 
