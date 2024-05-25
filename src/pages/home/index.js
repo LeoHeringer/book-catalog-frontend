b3033b52dfe834f7d10c27719ee781dbffdc844d
LeoHeringer <leonardoheringer2016@hotmail.com>
2024-05-25 00:58:42 -0300

    consumo

--- src/pages/home/index.js
+++ src/pages/home/index.js
@@ -1,41 +1,43 @@
 import { Container, BookList, Book } from "./styles.js";
+import { useEffect, useState } from "react";
 
-function Home () {
+function Home() {
+    const [books, setBooks] = useState([]);
+
+    useEffect(() => {
+        fetch("http://localhost:8000/rest/get-book?name=dracula")
+            .then(response => {
+                if (!response.ok) {
+                    throw new Error('Erro na resposta da rede');
+                }
+                return response.json();
+            })
+            .then(data => {
+                console.log(data);
+                setBooks(data.books || []);
+            })
+            .catch(error => console.error('Erro ao buscar os livros:', error));
+    }, []);
     
-    const books = [
-        {
-            id: 1,
-            title: 'Dracula',
-            image_url: 'https://m.media-amazon.com/images/I/51Mg0vMecsL._AC_UF1000,1000_QL80_.jpg'
-        },
-        {
-            id: 2,
-            title: 'Le',
-            image_url: 'https://m.media-amazon.com/images/I/51Mg0vMecsL._AC_UF1000,1000_QL80_.jpg'
-        },
-        {
-            id: 3,
-            title: 'Lu',
-            image_url: 'https://m.media-amazon.com/images/I/51Mg0vMecsL._AC_UF1000,1000_QL80_.jpg'       
-        }
-
-    ]
     return (
         <Container>
-        <h1>Books</h1>
-        <BookList>
-            {books.map(books => {
-                return (
-                    <Book>
-                        <a href="https://google.com.br"><img src={books.image_url} all={books.title}/></a>
-                        <span>{books.title}</span>
+            <h1>Books</h1>
+            <BookList>
+                {books.map((book, index) => (
+                    <Book key={index}>
+                        <a href="https://bibliotecasaavedra.com.br/products/dracula-bram-stoker-nova-cultural?variant=41965203489012&currency=BRL&utm_medium=product_sync&utm_source=google&utm_content=sag_organic&utm_campaign=sag_organic&gad_source=1&gclid=CjwKCAjw9cCyBhBzEiwAJTUWNe5HP1F4wfyoToz4f35Nxrx5ko4PznKxTiPhkAokQHIj8bFfqM9rphoCrnEQAvD_BwE">
+                            <img src={book.image_url} alt={book.title} />
+                        </a>
+                        <span>{book.title}</span>
                     </Book>
-                )
-            })}
-        </BookList>
+                ))}
+            </BookList>
+   
         </Container>
 
-    )
+    );
+    
+    
 }
 
 export default Home;
